d449fe7012965078dbca9854e5260a256f63375a
Taimur Shah <tshah@scu.edu>
2018-11-06 15:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//it helps to think about this first function as a real scenario. Anybody who takes a number is automatically at the end of the line. ALL I have to do is add their name to the line array, and then I have to tell them what their place is in line. since they're always going to be last in line, all I have to do is return the length of the line. 
+//it helps to think about this first function as a real scenario. Anybody who takes a number is automatically at the end of the line. ALL I have to do is add their name to the line array, and then I have to tell them what their place is in line. since they're always going to be last in line, all I have to do is use the length of the line. 
 //One test was "the function properly handles adding multiple people," but the argument "name" can only be one at a time, no?
-//figured it out -- what that test means is: multiple arguments can be passed through the 'name' parameter, and that name will always be added to the end of the array (hence array.push(name)). Basically it's testing to see whether I properly add people to the array; they are always at the end.
+//figured it out -- what that test means is: multiple arguments can be passed through the 'name' parameter, and that name will always be added to the end of the array (hence array.push(name)). Basically it's testing to see whether I properly add people to the array - that they are always at the end.
 
 function takeANumber(line, name) {
   line.push(name);
